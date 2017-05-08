be0646e0bf60f7cf4ebdf6e4cd86208b7bf56920
Balakrishna, Fnu <balakrishnafd@upmc.edu>
2017-05-08 14:57:59 -0400

    code clean up

--- src/js/custom.js
+++ src/js/custom.js
@@ -24,193 +24,6 @@ $(document).ready(function(e) {
 
     getDataFromAjax();
 
-    /*//front slider
-    if (typeof MasterSlider == 'function') {
-        front_slider = new MasterSlider();
-
-        front_slider.control('arrows', { autohide: false }); //add arrows
-        front_slider.control('bullets', { dir: 'v', autohide: false }); //add vertical bullets
-        front_slider.control('timebar', { autohide: false, align: 'top', color: '#01bab0' }); //add top timebar
-        front_slider.control('thumblist', { autohide: false, align: "bottom", inset: true, width: 150, height: 104 }); //add thumbnails
-
-        front_slider.setup('masterslider', {
-            width: 1400,
-            height: 800,
-            space: 0,
-            view: 'basic',
-            layout: 'fullscreen',
-            speed: 20,
-            autoplay: true,
-            overPause: false
-        });
-        front_slider.api.pause(); //pause slider, wait for page loader
-    }*/
-
-
-    //services slider
-    var slider_services = $('#slider-services');
-    if (slider_services.length == 1) {
-        slider_services.owlCarousel({
-            items: 3,
-            navigation: true,
-            navigationText: false,
-            pagination: false,
-            slideSpeed: 500,
-            itemsDesktop: [1199, 2],
-            itemsDesktopSmall: [979, 2],
-            itemsTablet: [768, 1]
-        });
-    }
-
-
-    //partners slider
-    var slider_partners = $('#slider-partners');
-    if (slider_partners.length == 1) {
-        slider_partners.owlCarousel({
-            items: 3,
-            navigation: true,
-            navigationText: false,
-            pagination: false,
-            slideSpeed: 500,
-            itemsDesktop: [1199, 3],
-            itemsDesktopSmall: [979, 3],
-            itemsTablet: [768, 2]
-        });
-    }
-
-
-    //blog slider
-    var blog_slider = $('#blog-slider');
-    if (blog_slider.length == 1) {
-        blog_slider.owlCarousel({
-            items: 3,
-            navigation: true,
-            navigationText: false,
-            pagination: false,
-            slideSpeed: 600,
-            itemsDesktop: [1599, 2],
-            itemsDesktopSmall: [900, 1],
-            itemsTablet: [768, 1]
-        });
-    }
-
-
-    //post sliders
-    var post_sliders = $('.post-preview-slider');
-    for (var i = 0, len = post_sliders.length; i < len; i++) {
-        post_sliders.eq(i).owlCarousel({
-            singleItem: true,
-            navigation: true,
-            navigationText: false,
-            slideSpeed: 400
-        });
-    }
-
-
-    //other sliders
-    var single_sliders = $('.single-slider');
-    for (var i = 0, len = single_sliders.length; i < len; i++) {
-        single_sliders.eq(i).owlCarousel({
-            singleItem: true,
-            navigation: true,
-            navigationText: false,
-            autoHeight: true,
-            slideSpeed: 400
-        });
-    }
-
-    var single_sliders_v2 = $('.single-slider-v2');
-    for (var i = 0, len = single_sliders_v2.length; i < len; i++) {
-        single_sliders_v2.eq(i).owlCarousel({
-            singleItem: true,
-            navigation: true,
-            navigationText: false,
-            pagination: false,
-            autoHeight: true,
-            slideSpeed: 400
-        });
-    }
-
-
-    //input focus/blur functionality
-    var input_containers = $('.input-container');
-    for (var i = 0, len = input_containers.length; i < len; i++) {
-        inputContainerCheck(input_containers.eq(i).find('input, textarea'), input_containers.eq(i));
-    }
-
-    function inputContainerCheck(input, container) {
-        input.on('focus blur', function(e) {
-            container.toggleClass('focus');
-        });
-    }
-
-
-    //bar graphs
-    var graph_anim_duration = 300;
-    var graph_bar = $('.graph-bar');
-    var current_graph_bar;
-    for (var i = 0, len = graph_bar.length; i < len; i++) {
-        var current_graph_bar = graph_bar.eq(i);
-        if (current_graph_bar.hasClass('onscroll-animate'))
-            continue;
-        setGraphBar(current_graph_bar);
-    }
-
-    function setGraphBar(graph_el) {
-        graph_el.find('.graph-line-value').css('width', graph_el.data('percentage') + '%');
-    }
-
-
-    //donut graphs
-    var graph_donut = $('.graph-donut');
-    for (var i = 0, len = graph_donut.length; i < len; i++) {
-        var current_graph_donut = graph_donut.eq(i);
-        if (current_graph_donut.hasClass('onscroll-animate'))
-            continue;
-        setGraphDonut(current_graph_donut);
-    }
-
-    function setGraphDonut(graph_el) {
-        var percentage = graph_el.data('percentage') / 100;
-        if (percentage < 0.5) {
-            graph_el.find('.graph-left .graph-inner').hide();
-            graph_el.find('.graph-right .graph-inner').css('transform', 'rotate(' + (180 + 360 * percentage) + 'deg)');
-        } else {
-            graph_el.find('.graph-right .graph-inner').css('transform', 'rotate(360deg)');
-            graph_el.find('.graph-left .graph-inner').css('transform', 'rotate(' + (360 * percentage) + 'deg)');
-        }
-    }
-
-
-    // on-scroll animations
-    var on_scroll_anims = $('.onscroll-animate');
-    for (var i = 0, len = on_scroll_anims.length; i < len; i++) {
-        var element = on_scroll_anims.eq(i);
-        element.one('inview', function(event, visible) {
-            var el = $(this);
-            var anim = (el.data("animation") !== undefined) ? el.data("animation") : "fadeIn";
-            var delay = (el.data("delay") !== undefined) ? el.data("delay") : 200;
-
-            var timer = setTimeout(function() {
-                el.addClass(anim);
-                clearTimeout(timer);
-            }, delay);
-
-            //for graphs
-            if (el.hasClass('graph-bar')) {
-                var graph_timer = setTimeout(function() {
-                    setGraphBar(el);
-                    clearTimeout(graph_timer);
-                }, delay + 700);
-            } else if (el.hasClass('graph-donut')) {
-                var graph_timer = setTimeout(function() {
-                    setGraphDonut(el);
-                    clearTimeout(graph_timer);
-                }, delay);
-            }
-        });
-    }
-
 
     //one page menu and highlight current menu item
     var one_page_nav = $('#one-page-nav');
@@ -305,104 +118,12 @@ $(document).ready(function(e) {
         openPopup(popup);
     });
 
-    $('.popup-window-next, .popup-window-prev').on('click', function(e) {
-        e.preventDefault();
-        var parent_popup = $(this).parents('.popup-window-container');
-        var new_popup;
-        if ($(this).hasClass('popup-window-next'))
-            new_popup = parent_popup.next('.popup-window-container');
-        else
-            new_popup = parent_popup.prev('.popup-window-container');
-        if (new_popup.length == 1) {
-            closePopups(false);
-            if (!Modernizr.csstransitions) {
-                openPopup(new_popup);
-            } else {
-                parent_popup.one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
-                    openPopup(new_popup);
-                });
-            }
-        }
-    });
-
-    $('.popup-window-closing-area, .popup-window-close, .popup-window-close-trigger, #page-screen-cover').on('click', function(e) {
-        e.preventDefault();
-        closePopups();
-    });
-
-    function openPopup(popup_window) {
-        if (popup_window.height() + $(window).scrollTop() > $(document).height()) {
-            $.scrollTo(0, 300, { axis: 'y' });
-            popup_window.css('top', 0);
-        } else
-            popup_window.css('top', $(window).scrollTop());
-        popup_window.addClass('active');
-        page_screen_cover.addClass('active');
-    }
-
-    function closePopups(clear_screen) {
-        popup_windows.removeClass('active');
-        if (clear_screen == false)
-            return;
-        page_screen_cover.removeClass('active');
-    }
-
-
-    //portfolio layout6, layout7, layout8 hover
-    function portfolioLayoutImg(img_el) {
-        var hidden_part = img_el.find('.portfolio-img-detail-hidden');
-        img_el.on('mouseenter', function() {
-                hidden_part.stop().slideDown();
-            })
-            .on('mouseleave', function() {
-                hidden_part.stop().slideUp();
-            });
-    }
-
-    var portfolio_layouts_imgs = $('.portfolio-layout6 .portfolio-img, .portfolio-layout7 .portfolio-img, .portfolio-layout8 .portfolio-img');
-    for (var i = 0, len = portfolio_layouts_imgs.length; i < len; i++) {
-        portfolioLayoutImg(portfolio_layouts_imgs.eq(i));
-    }
-
-
     //tooltips
     $('[data-toggle="tooltip"]').tooltip();
 
 
-    googleMap();
-
-    /* Google Map */
-    function googleMap() {
-        var map_canvas = $('#map-canvas');
-        if (map_canvas.length == 0)
-            return;
-        var map;
-        var myLatlng = new google.maps.LatLng(40.714728, -73.998672);
-        var center = new google.maps.LatLng(40.714728, -74.050672);
-
-        function mapInitialize() {
-            var mapOptions = {
-                scrollwheel: false,
-                zoom: 12,
-                center: center
-            };
-            map = new google.maps.Map(map_canvas.get(0), mapOptions);
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map
-            });
-        }
-        google.maps.event.addDomListener(window, 'load', mapInitialize);
-    }
 
 
-    //contact forms
-    $('#form-contact').on('submit', function(e) {
-        return form_to_ajax_request($(this), ['name', 'email', 'phone', 'message'], ['email', 'message']);
-    });
-    $('#form-contact2').on('submit', function(e) {
-        return form_to_ajax_request($(this), ['name', 'email', 'phone', 'message'], ['email', 'message']);
-    });
 });
 
 var loadMasterSlider = function() {
@@ -462,38 +183,6 @@ $(window).load(function(e) {
     }
 
 
-    //portfolio layout 2 - isotope activation
-    /*var portfolio2 = $('#portfolio2');
-    if (portfolio2.length == 1) {
-        var isotopeEl2 = portfolio2.isotope({
-            itemSelector: '.portfolio-item',
-            masonry: {
-                columnWidth: '.portfolio-item'
-            },
-            percentPosition: true
-        });
-
-        //isotope filter
-        var isotope_filter2_list = $('#isotope-filter-list2');
-        var isotope_filter2_items = isotope_filter2_list.find('.isotope-filter');
-        var isotope_filter2_all = isotope_filter2_list.find('[data-filter="*"]');
-        var automatic_filtering = portfolio2.hasClass('filtering-on-button') ? false : true;
-        isotope_filter2_items.on('click', function(e) {
-            e.preventDefault();
-            if ($(this).get(0) == isotope_filter2_all.get(0)) {
-                isotope_filter2_items.removeClass('active');
-                isotope_filter2_all.addClass('active');
-            } else {
-                isotope_filter2_all.removeClass('active');
-                $(this).toggleClass('active');
-            }
-            if (automatic_filtering)
-                portfolio2_filter_perform();
-        });
-        isotope_filter2_list.find('.isotope-filter-button > a').on('click', function(e) {
-            portfolio2_filter_perform();
-        });
-    }*/
 
     function portfolio2_filter_perform() {
         var filter_string = '';
@@ -780,8 +469,18 @@ $.fn.masonryImagesReveal = function($items) {
 
 function setUpFancybox() {
     $(".fancybox").fancybox({
-        openEffect: 'none',
-        closeEffect: 'none'
+        prevEffect: 'none',
+        nextEffect: 'none',
+        helpers: {
+            title: {
+                type: 'outside'
+            },
+            thumbs: {
+                width: 50,
+                height: 50
+            }
+        }
+
     });
 
 }
@@ -810,7 +509,7 @@ var loadHomePhotos = function(photoJsonData) {
                 /*var template = '<article class="col-xxxl-12-5 col-xxl-3 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-12 portfolio-item filter-people"> <div class="portfolio-item-content"><div class="portfolio-img"> <img alt="image" src="http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '"> <div class="portfolio-img-detail"><div class="portfolio-img-detail-inner"> <a class="portfolio-img-detail-content" href="http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '" data-lightbox="portfolio-images"></a> </div> </div> </div><div class="">  <a class="popup-window-trigger" data-popup="#popup-portfolio-' + photoJsonData.item[i].imgs.img[j].id + '"></a> </div> </div> </article>' +
                     '<section id="popup-portfolio-' + photoJsonData.item[i].imgs.img[j].id + '" class="popup-window-container"><div class="section-content"> <div class="popup-window-closing-area"></div><div class="container"> <div class="popup-window portfolio-work-layout2"> <div class="popup-window-close popup-window-close-light popup-window-close-small"></div> <div class="portfolio-work-img"> <div class="single-slider black-arrows"> <a href="http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '" data-lightbox="portfolio-item1-images"> <img alt = "image"  src = "http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '" > </a> </div> </div> </div> </div> </div>     </section>';*/
                 //var template = ' <div class="item"> <img src="http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '"> </div>'
-                var template = '<div class="item"> <a class="fancybox" rel="gallery1" href="http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '"> <img  alt="" src="http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '" />  </a> </div>'
+                var template = '<div class="item"> <a class="fancybox" rel="fancybox" href="http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '"> <img  alt="" src="http://drive.google.com/uc?export=view&id=' + photoJsonData.item[i].imgs.img[j].id + '" />  </a> </div>'
                     //  var template = '<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm6.staticflickr.com/5444/17679973232_568353a624_b.jpg" title="Golden Manarola (Sanjeev Deo)">	<img src="http://farm6.staticflickr.com/5444/17679973232_568353a624_m.jpg" alt="" /></a><a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm8.staticflickr.com/7367/16426879675_e32ac817a8_b.jpg" title="Codirosso spazzacamino (Massimo Greco _Foligno)">	<img src="http://farm8.staticflickr.com/7367/16426879675_e32ac817a8_m.jpg" alt="" /></a><a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm6.staticflickr.com/5612/15344856989_449794889d_b.jpg" title="Morning Twilight (Jose Hamra Images)">	<img src="http://farm6.staticflickr.com/5612/15344856989_449794889d_m.jpg" alt="" /></a><a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm8.staticflickr.com/7289/16207238089_0124105172_b.jpg" title="(Eric Goncalves (cathing up again!))">	<img src="http://farm8.staticflickr.com/7289/16207238089_0124105172_m.jpg" alt="" /></a>'
                 $('.portfolio-nature').append(template);
             }
